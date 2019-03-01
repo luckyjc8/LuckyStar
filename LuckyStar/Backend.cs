0af7d609b81e221fb081a9ebe6b831fbedaf7d00
Lucky.JC <lucky.jonathan.lc@gmail.com>
2019-03-01 18:26:30 +0700

    added readMap method to new class Backend

---
+++ LuckyStar/Backend.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuckyStar
+{
+    class Backend
+    {
+        LinkedList<int>[] public readMap(string path)
+        {
+            int houses;
+            LinkedList<int>[] paths;
+
+            string text = File.ReadAllText(@path, Encoding.UTF8);
+            string[] txt = text.Split('\n');
+            bool first = true;
+            foreach (string input in txt)
+            {
+                if (first)
+                {
+                    houses = Int32.Parse(input);
+                    first = false;
+                }
+                else
+                {
+                    inputs = input.Split(" ");
+                    paths[Int32.Parse(input[0])].AddLast(Int32.Parse(input[1]))
+                }
+            }
+        }
+    }
+}
